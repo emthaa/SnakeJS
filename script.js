3c6483e30a809c881c1082f565876c6950c95fc4
emthaa <110277522+emthaa@users.noreply.github.com>
2023-05-19 17:40:36 +0000

    finished all features

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
 let board = document.querySelector("#board")
 let content = document.querySelector('#content')
-let board_size = 4 
+let board_size = 6
 let boardArr = []
-let con = document.querySelector('#console')
 let deadZoneX = []
 let deadZoneY = []
+let gameState = true
 const squares = board_size * board_size
-function calculateDeadZone(){
-  for(i = -1;i<board_size;i++){
+
+function calculateDeadZone() {
+  for (i = -1; i < board_size; i++) {
     deadZoneY.push(-1)
     deadZoneX.push(i)
   }
-  for(i = -1;i<board_size;i++){
+  for (i = -1; i < board_size; i++) {
     deadZoneY.push(board_size)
     deadZoneX.push(i)
   }
-  for(i = -1;i<board_size;i++){
+  for (i = -1; i < board_size; i++) {
     deadZoneY.push(i)
     deadZoneX.push(-1)
   }
-  for(i = -1;i<board_size;i++){
+  for (i = -1; i < board_size; i++) {
     deadZoneY.push(i)
     deadZoneX.push(board_size)
   }
@@ -30,8 +31,9 @@ function calculateDeadZone(){
 calculateDeadZone()
 
 
-//TO DO: , SCORE, -> GAME OVER , GAME WIN 
-//BUGS: 
+//TO DO: 
+//BUGS:
+
 
 board.style.gridTemplateColumns = `repeat(${board_size},1fr)`;
 board.style.gridTemplateRows = `repeat(${board_size},1fr)`;
@@ -56,7 +58,7 @@ function clearBoard() {
   for (let y = 0; y < board_size; y++) {
     for (let x = 0; x < board_size; x++) {
       drawApple()
-      boardArr[y][x].style.backgroundColor = 'white'
+      boardArr[y][x].style.backgroundColor = 'black'
       boardArr[y][x].style.border = 'none'
 
     }
@@ -77,49 +79,55 @@ class snakeBod {
 
 
 
-let snakeHead = new snakeBod(0, 0, 0, 0, 0, 0)
+let snakeHead = new snakeBod(2, 2, 0, 0, 0, 0)
 let snakeHeadVis = boardArr[snakeHead.currYpos][snakeHead.currXpos]
 let snakeBody = [snakeHead]
 let snakeBodyVis = [snakeHeadVis]
+
 snakeHeadVis.style.backgroundColor = 'rgb(0,200,0)'
 snakeHeadVis.style.border = '3px solid black'
-snakeBodyVis[0].style.backgroundColor = "rgb(0,200,0)"
-snakeBodyVis[0].style.border = '3px solid black'
+let check1 = false
+let check2 = false
 
 function drawSnake(xpos, ypos) {
   snakeBody[0].currXpos += xpos
   snakeBody[0].currYpos += ypos
-  deadZoneCheckCollision()
-  
+  if (check1 == true || check2 == true) {
+    return
+  }
+  check1 = deadZoneCheckCollision()
+
   for (i = 0; i < snakeBody.length; i++) {
 
 
 
     snakeBodyVis[i] = boardArr[snakeBody[i].currYpos][snakeBody[i].currXpos]
 
-    snakeBodyVis[0].style.backgroundColor = "rgb(0,200,0)"
-    snakeBodyVis[0].style.border = '3px solid black'
+
     snakeBodyVis[i].style.backgroundColor = 'green'
     snakeBodyVis[i].style.border = '3px solid black'
-    
+    snakeBodyVis[0].style.backgroundColor = "rgb(0,200,0)"
+    snakeBodyVis[0].style.border = '3px solid black'
     if (snakeBody[i + 1]) {
-      
+
       snakeBody[i + 1].currXpos = snakeBody[i].lastXpos;
       snakeBody[i + 1].currYpos = snakeBody[i].lastYpos;
+
       console.log('tail', snakeBody[i + 1], 'i', i + 1)
-      snakeBodyVis[i + 1] = boardArr[snakeBody[i + 1].currYpos][snakeBody[i + 1].currXpos] 
+      snakeBodyVis[i + 1] = boardArr[snakeBody[i + 1].currYpos][snakeBody[i + 1].currXpos]
       snakeBodyVis[i + 1].style.backgroundColor = 'green'
-  
+
     }
-    
+
     snakeBody[i].lastXpos = snakeBody[i].currXpos;
     snakeBody[i].lastYpos = snakeBody[i].currYpos;
-    
+
   }
-  checkBodyCollision()
+  check2 = checkBodyCollision()
 }
 
-function gameOver(){
+function gameOver() {
+  gameState = false;
   const GameOverScreen = document.createElement('div')
   GameOverScreen.id = 'GameOverScreen'
   content.appendChild(GameOverScreen)
@@ -127,26 +135,49 @@ function gameOver(){
   const GameOverText = document.createElement('div')
   GameOverText.id = 'GameOverText'
   GameOverScreen.appendChild(GameOverText)
-  GameOverText.innerHTML = 'Game Over Idiot'
+  GameOverText.innerHTML = 'Game Over'
+
+  const gameOverScore = document.createElement('div')
+  gameOverScore.id = 'GameOverScore'
+  GameOverScreen.appendChild(gameOverScore)
+  gameOverScore.innerHTML = `Score: ${snakeBody.length * 100 - 100}`
+
+  const gameOverButton = document.createElement('button')
+  gameOverButton.id = 'gameOverButton'
+  GameOverScreen.appendChild(gameOverButton)
+  gameOverButton.innerHTML = 'Play Again'
+  gameOverButton.addEventListener('click', function() {
+    location.reload()
+  })
 }
 
+function checkWin(){
+  if(snakeBody.length == squares){
+    win()
+  }
+}
 
-function checkBodyCollision(){
-  for(i=1;i<snakeBody.length;i++){  
-    if(snakeHead.currXpos == snakeBody[i].currXpos && snakeHead.currYpos == snakeBody[i].currYpos){
-    gameOver()
+function win(){
+  gameState = false
+}
+
+function checkBodyCollision() {
+  for (i = 1; i < snakeBody.length; i++) {
+    if (snakeHead.currXpos == snakeBody[i].currXpos && snakeHead.currYpos == snakeBody[i].currYpos) {
+      gameOver()
+      return true
     }
 
   }
-  
+
 
 }
 
-
-function deadZoneCheckCollision(){
-  for(i=0;i<deadZoneX.length;i++){
-    if(snakeHead.currXpos == deadZoneX[i] && snakeHead.currYpos == deadZoneY[i]){
+function deadZoneCheckCollision() {
+  for (i = 0; i < deadZoneX.length; i++) {
+    if (snakeHead.currXpos == deadZoneX[i] && snakeHead.currYpos == deadZoneY[i]) {
       gameOver()
+      return true
     }
   }
 }
@@ -161,46 +192,47 @@ function linkSnake() {
 }
 
 function growSnake() {
-  let tail = snakeBody[snakeBody.length - 1]; 
+  let tail = snakeBody[snakeBody.length - 1];
   newTail = new snakeBod(tail.lastXpos, tail.lastYpos)
   snakeBody.push(newTail)
-  console.log(snakeBody)
 }
 
 function findAppleCord() {
-  let foundSpot = false
-  while (foundSpot == false) {
+  while (true) {
     yApplePos = Math.floor(Math.random() * board_size);
     xApplePos = Math.floor(Math.random() * board_size);
 
-    for(i = 0;i<snakeBody.length;i++){
-      if(snakeBody[i].currXpos == xApplePos && snakeBody[i].currYpos == yApplePos){
+    for (i = 0; i < snakeBody.length; i++) {
+      if (snakeBody[i].currXpos == xApplePos && snakeBody[i].currYpos == yApplePos) {
         findAppleCord()
       }
     }
 
-      return [xApplePos, yApplePos]
-    
+    return [xApplePos, yApplePos]
+
 
   }
-  
+
 }
-
-
 
 function drawApple() {
-  if(snakeBody.length != squares){
-  apple = boardArr[cords[1]][cords[0]]
-  apple.style.backgroundColor = 'red'
-  apple.style.border = '3px solid black'
+  if (snakeBody.length != squares) {
+    apple = boardArr[cords[1]][cords[0]]
+    apple.style.backgroundColor = 'red'
+    apple.style.border = '3px solid black'
 
-  if (snakeBody[0].currYpos == cords[1] && snakeBody[0].currXpos == cords[0]) {
-    growSnake()
-    cords = findAppleCord()   
-    drawApple()
-  }
+    if (snakeBody[0].currYpos == cords[1] && snakeBody[0].currXpos == cords[0] && gameState != false) {
+      growSnake()
+      
+      checkWin()
+      cords = findAppleCord()
+      drawApple()
+    }
   }
 }
+
+
+
 
 canMoveUp = true
 canMoveDown = true
@@ -213,8 +245,7 @@ function moveUp() {
   snakeBody[0].lastXpos = snakeBody[0].currXpos
   snakeBody[0].lastYpos = snakeBody[0].currYpos
   drawSnake(0, -1)
-
-  canMoveUp = false 
+  canMoveUp = false
   canMoveDown = false
   canMoveLeft = true
   canMoveRight = true
@@ -258,7 +289,7 @@ function moveLeft() {
   canMoveRight = false
 }
 
-const speed = 400
+const speed = 200
 let intervalUp
 let intervalRight
 let intervalLeft
@@ -266,32 +297,32 @@ let intervalDown
 cords = findAppleCord()
 
 
-drawSnake(snakeHead.currXpos, snakeHead.currYpos)
+
 drawApple()
 
 document.addEventListener('keydown', function(event) {
 
-  if (event.keyCode == 87 && canMoveUp == true) { //up
+  if (event.keyCode == 87 && canMoveUp == true && gameState != false) { //up
     clearInterval(intervalLeft);
     clearInterval(intervalRight);
     clearInterval(intervalDown);
     moveUp()
     intervalUp = setInterval(moveUp, speed)
 
-  } else if (event.keyCode == 68 && canMoveRight == true) {
+  } else if (event.keyCode == 68 && canMoveRight == true && gameState != false) {
     clearInterval(intervalUp);
     clearInterval(intervalLeft);
     clearInterval(intervalDown);
     moveRight()
     intervalRight = setInterval(moveRight, speed)
 
-  } else if (event.keyCode == 65 && canMoveLeft == true) {
+  } else if (event.keyCode == 65 && canMoveLeft == true && gameState != false) {
     clearInterval(intervalUp);
     clearInterval(intervalRight);
     clearInterval(intervalDown);
     moveLeft()
     intervalLeft = setInterval(moveLeft, speed)
-  } else if (event.keyCode == 83 && canMoveDown == true) {
+  } else if (event.keyCode == 83 && canMoveDown == true && gameState != false) {
     clearInterval(intervalUp);
     clearInterval(intervalRight);
     clearInterval(intervalLeft);
